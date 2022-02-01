6efbbe495df5c977006371070a07f8335608d42b
unknown <sachin raut>
2022-02-01 12:30:23 +0530

    intial commit

---
+++ src/main/java/com/aster/userServices/controller/HelloWordController.java
@@ -0,0 +1,13 @@
+package com.aster.userServices.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWordController {
+
+	@GetMapping("/hello")
+	public String helloMessage() {
+		return "Welcome To Spring Boot Application !!!!!";
+	}
+}
